604f322722f64341b8e645942773e1d9e093cf51
df7c5117 <df7c5117@gmail.com>
2018-03-25 17:14:38 +0800

    renamed to toocheer

--- js/pages/index.js
+++ js/pages/index.js
@@ -5,17 +5,12 @@ $(document).ready(function(){
 		if(scrollTop > top){                 //滑动到该位置时执行代码
 			$(".navbar").removeClass("transparent");
 			$("#backtotop").removeClass("hide");
+			$("#dashboard").removeClass("hide");
 		}else{
 			$(".navbar").addClass("transparent");
 			$("#backtotop").addClass("hide");
+			$("#dashboard").addClass("hide");
 		}
 	});
 });
 function backtop(){$("body").animate({scrollTop:0})}
-
-function dispwx(){
-	$("#wx").removeClass('hide');
-}
-function hidewx(){
-	$("#wx").addClass('hide');
-}
