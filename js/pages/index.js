eba0e647c3757dcce6a7cbc605e5f2cd88674962
df7c5117 <df7c5117@gmail.com>
2018-04-21 22:00:51 +0800

    ui improve

--- js/pages/index.js
+++ js/pages/index.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
 		var scrollTop = $(window).scrollTop();  //获取当前滑动位置
 		if(scrollTop > top){                 //滑动到该位置时执行代码
 			$(".navbar").removeClass("transparent");
+			$(".navbar").addClass("blur");
 			$("#backtotop").removeClass("hide");
 			$("#dashboard").removeClass("hide");
 		}else{
 			$(".navbar").addClass("transparent");
+			$(".navbar").removeClass("blur");
 			$("#backtotop").addClass("hide");
 			$("#dashboard").addClass("hide");
 		}
